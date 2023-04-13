2d8af3347a04afc73b6a7e27c58680d3957a8dcc
Nitish Kumar <myhobbyissolvemath@gmail.com>
2023-04-13 16:49:34 +0530

    Project Created

---
+++ src/main/java/com/nitish/project/spring/ShoppingCartServicesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nitish.project.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCartServicesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCartServicesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nitish/project/spring/ShoppingCartServicesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nitish.project.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartServicesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
