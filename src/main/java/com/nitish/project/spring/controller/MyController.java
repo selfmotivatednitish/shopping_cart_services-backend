7d9e856e48aa03a191ee0a3601ea3b64213b3e23
Nitish Kumar <myhobbyissolvemath@gmail.com>
2023-04-19 16:02:10 +0530

    Cros Origin added

--- src/main/java/com/nitish/project/spring/controller/MyController.java
+++ src/main/java/com/nitish/project/spring/controller/MyController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,6 +30,7 @@ import com.nitish.project.spring.services.ProductService;
 import com.nitish.project.spring.services.UserService;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class MyController {
 
 	@Autowired
@@ -60,6 +62,7 @@ public class MyController {
 	}
 
 	// add product
+	
 	@PostMapping("/products/addProduct")
 	public Product addProduct(@RequestBody Product product) {
 		return this.productService.addProduct(product);
